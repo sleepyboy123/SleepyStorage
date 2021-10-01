85e9b7f1b15a4a95a20e1e7c97ab9e00d1e2289e
sleepyboy <sleepyboy123@hotmail.com>
2021-10-01 14:59:41 +0800

    Initial Commit React Done

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sleepy Storage
+
+## To Do
+- Set up local IPFS Node
+- Pay IPFS Node
+- Display all photos for current account and display
+- Find a way to check for proof
+- Calculate amount to be paid?
+- Store data redundantly across multiple hosts
+- Periodically submit proof of their continued storage until contract expires
+- Host is compensated for every proff they submit, penalized for missing proof
--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,68 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
+import { create } from 'ipfs-http-client'
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null,
+      imageHash: 'QmenyRPcPjghG1RJ3PVSFVkyhEMaDxnHr8LFsbhV7httKN'
+    }
+  }
+
+  // Example Path: QmenyRPcPjghG1RJ3PVSFVkyhEMaDxnHr8LFsbhV7httKN
+  getFile = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result)
+      })
+    }
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const added = await client.add(this.state.buffer)
+      this.setState({
+        imageHash: added.path
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://sleepytyper.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Sleepy Storage
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+
+                <img style={{height: 200, width: 200}} src={'https://ipfs.infura.io/ipfs/' + this.state.imageHash} className="App-logo" alt="logo" />
+                <h2 style={{paddingTop: 15, paddingBottom: 15}}>Upload File</h2>
+                <form onSubmit={this.onSubmit}>
+                  <input type="file" onChange={this.getFile}/>
+                  <input type="submit"/>
+                </form> 
               </div>
             </main>
           </div>
